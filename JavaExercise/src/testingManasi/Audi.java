245f138657d7587f17ae05b34f7007c4f19b3704
Manasim <Manasi.mahabaleshwarkar@bitwiseglobal.net>
2016-06-23 19:10:33 +0530

    commit

---
+++ JavaExercise/src/testingManasi/Audi.java
@@ -0,0 +1,13 @@
+package testingManasi;
+
+public class Audi extends Car {
+	
+	public Audi (int speed, int gears, String model)
+	{
+		this.speed=speed;
+		this.gears=gears;
+		this.model=model;
+			
+	}
+
+}
---
+++ JavaExercise/src/testingManasi/Jaguar.java
@@ -0,0 +1,20 @@
+package testingManasi;
+
+public class Jaguar extends Car 
+{
+		public Jaguar (int speed, int gears, String model)
+	{
+		this.speed=speed;
+		this.gears=gears;
+		this.model=model;
+			
+	}
+
+		public Jaguar (String model)
+		{
+			
+			this.model=model;
+				
+		}
+}
+
