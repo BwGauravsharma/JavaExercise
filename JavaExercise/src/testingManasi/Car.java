96e2b524cfd79a93674c0a8407921619389e0171
Manasim <Manasi.mahabaleshwarkar@bitwiseglobal.net>
2016-06-23 19:10:06 +0530

    Assignment 3

---
+++ JavaExercise/src/testingManasi/Car.java
@@ -0,0 +1,25 @@
+package testingManasi;
+
+public class Car 
+
+{
+	int speed, gears;
+	String model;
+
+	
+	
+	public void displayCar ()
+	{
+		System.out.println ("Speed Of the Vechile is " + speed);
+		System.out.println ("Speed Of the Vechile is " + gears);
+		
+		System.out.println ("Model Of the Vechile is " + model);
+	}
+  	
+	public void displayModel ()
+	{
+				
+		System.out.println ("Model Of the Vechile is " + model);
+	}
+	
+}
---
+++ JavaExercise/src/testingManasi/TestJava.java
@@ -0,0 +1,13 @@
+package testingManasi;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	System.out.println("Hi!!!");	
+
+	}
+
+}
