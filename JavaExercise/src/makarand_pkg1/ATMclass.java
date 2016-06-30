eef4945bfa8c88609e81e5e369ce4b3d742ffcaa
makarand17 <mak7ksb@gmail.com>
2016-06-30 21:16:44 +0530

    Commit by Makarand-30-June

---
+++ JavaExercise/src/makarand_pkg1/ATMclass.java
@@ -0,0 +1,8 @@
+package makarand_pkg1;
+
+abstract  class ATMclass {
+	
+	 abstract void withdrawFromAccount(int amount, String accountType);
+	
+	abstract void displayStatement(String accountType);
+}
