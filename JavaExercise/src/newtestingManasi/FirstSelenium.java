8bb43a0dc465c5943c93945a435ab08c78cc7179
Manasim <Manasi.mahabaleshwarkar@bitwiseglobal.net>
2016-07-05 15:27:26 +0530

    commit

--- JavaExercise/src/newtestingManasi/FirstSelenium.java
+++ JavaExercise/src/newtestingManasi/FirstSelenium.java
@@ -2,22 +2,29 @@ package newtestingManasi;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.MarionetteDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 //import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstSelenium 
 
 {
+	static WebDriver driver;
 
 	public static void main(String[] args) 
 	{
-		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
-		capabilities.setCapability("marionette", true);
-		WebDriver driver = new FirefoxDriver(capabilities);
+		
+		System.setProperty("webdriver.gecko.driver","C:\\drivers\\marionette\\wires-0.6.2-win.exe");
+			driver = new MarionetteDriver();
+
+		
+//		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+//		capabilities.setCapability("marionette", true);
+//		WebDriver driver = new FirefoxDriver(capabilities);
 		driver.manage().window().maximize();
 		driver.get("https://google.co.in");
 		driver.findElement(By.id("lst-ib")).sendKeys("Automation is a fun");
-//		driver.findElement(By.className("sbico")).click();
+        driver.findElement(By.id("sbico")).click();
 		
 		//driver.findElement(By.xpath("//*[@id=sblsbb]/button/span")).click();
 		
